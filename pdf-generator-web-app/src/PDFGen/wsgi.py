2a213f11c76be0f6fce48863f55880cb0bc8a0ec
Ryan Jones <ryan.j1301@icloud.com>
2020-11-21 23:11:55 +0000

    Revamp Project Structure

--- pdf-generator-web-app/src/PDFGen/wsgi.py
+++ pdf-generator-web-app/src/PDFGen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pdf_generator project.
+WSGI config for PDFGen project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_generator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PDFGen.settings')
 
 application = get_wsgi_application()
