983a373844bfc458fccedc3cec59261ab13ba077
Ryan Jones <ryan.j1301@icloud.com>
2020-11-23 05:14:57 +0000

    Project Structure Revamp

--- pdf-generator-web-app/manage.py
+++ pdf-generator-web-app/manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from pathlib import Path
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PDFGen.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # For placing apps inside project directory
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "pdfgen"))
+
     execute_from_command_line(sys.argv)
 
 
